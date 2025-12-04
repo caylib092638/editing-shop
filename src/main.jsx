d3e7e4a0c2d116c9c281a4f920060ecb552afd08
unknown <yamaguchikunjooeet@gmail.com>
2025-12-04 15:26:20 +0800

    Updated files and fixed admin duplicates

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";      // customer page
-import Admin from "./Admin.jsx";  // admin page
+import Admin from "./pages/AdminDashboard.jsx";  // admin page
 import QRCodePage from "./QRCodePage.jsx";
 
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/admin" element={<AdminDashboard />} />
       <Route path="/qr" element={<QRCodePage />} />
 
     </Routes>
